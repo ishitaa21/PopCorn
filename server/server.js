136407a7856f722b2d9c93b8583bc607a375db4d
Ishita <ishitasarkar400@gmail.com>
2026-03-17 22:29:52 +0530

    fix: reorder middleware to allow inngest webhooks

--- server/server.js
+++ server/server.js
@@ -17,12 +17,13 @@ await connectDB()
 //middleware => essentially a function that sits between the incoming request and the final route handler
 app.use(express.json())
 app.use(cors())
-app.use(clerkMiddleware())
+
 
 
 //API routes
 app.get('/',(req,res)=> res.send('server is live!'))
 app.use('/api/inngest',serve({ client: inngest, functions }))
 
+app.use(clerkMiddleware())
 
 app.listen(port, ()=> console.log(`server listening at http://localhost:${port}`));
